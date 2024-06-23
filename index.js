3454bf22486e6e021a5e3fec7bd92d19bd955118
rajwindersxxx <rajwindersxxx@gmail.com>
2024-06-23 17:11:43 +0530

    basic layout template

--- index.js
+++ index.js
@@ -9,9 +9,26 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res)=> {
-    res.render("index.ejs");
+    res.render("today.ejs");
 })
-
+app.get("/today", (req, res)=> {
+    res.render("today.ejs");
+})
+app.get("/hourly", (req, res)=> {
+    res.render("hour.ejs");
+})
+app.get("/monthly", (req, res)=> {
+    res.render("month.ejs");
+})
+app.get("/weekend", (req, res)=> {
+    res.render("weekend.ejs");
+})
+app.get("/radar", (req, res)=> {
+    res.render("radar.ejs");
+})
+app.get("/find", (req, res)=> {
+    res.render("find.ejs");
+})
 app.listen(port , () =>{
     console.log(`server is running on localhost:${port}`)
 })
